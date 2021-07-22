ccda221f473a9fb933e2b2a5f3dbfabc1a818688
Andrew <maple33g@gmail.com>
2021-07-22 17:13:43 +0300

    Scaffolded identity for login in and registration set up

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using PersonalBlog.Models;
 
 namespace PersonalBlog.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
--- Startup.cs
+++ Startup.cs
@@ -12,6 +12,7 @@ using PersonalBlog.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using PersonalBlog.Models;
 
 namespace PersonalBlog
 {
@@ -30,7 +31,7 @@ namespace PersonalBlog
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews().AddRazorRuntimeCompilation();
             services.AddRazorPages();
